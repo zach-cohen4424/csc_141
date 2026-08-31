224c51d4305b44759505c489a12c80531e0c4b32
zach-cohen4424 <zachary.cohen001@albright.edu>
2026-08-31 13:51:02 -0400

    1st

---
+++ assignments/01_getting_started/01_01_python_org.py
@@ -0,0 +1,4 @@
+"""
+I have looked at python.org and have learned some cool things. 
+
+"""
---
+++ assignments/01_getting_started/01_02_hello_world_typos.py
@@ -0,0 +1,3 @@
+print("Hello Python World!")
+print("hello_world.py")
+# printx("Hello Python Crash Course world!")
