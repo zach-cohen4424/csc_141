3b181ba9d0ce20a6d39e705470c0819a5ab0c01c
zach-cohen4424 <zachary.cohen001@albright.edu>
2026-09-08 10:55:58 -0400

    csc_141

---
+++ Chapter 2/02_11_adding_comments.py
@@ -0,0 +1,9 @@
+"""
+msg = "I love Computer Science. It is a fascinating field with endless possibilities."
+print(msg)
+"""
+"""
+favorite_number = 24
+message = f"My favorite number is {favorite_number}."
+print(message)
+"""
---
+++ Chapter 2/02_12_zen_of_python.py
@@ -0,0 +1,3 @@
+import this
+print("This is the Zen of Python, by Tim Peters.")
+print(this.s)
